143db3dcf646b25549933fda0cf79a4aab9f8296
fabrice ferrere <fabrice.ferrere@ipilyon.net>
2021-01-13 01:11:48 +0100

    First commit initialisation du projet mobile , Introduction + premier activity (menu)

---
+++ app/src/main/java/com/rage/quizzmesiapps/MainActivity.java
@@ -0,0 +1,19 @@
+package com.rage.quizzmesiapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // cache la action bar
+        getSupportActionBar().hide();
+    }
+
+
+
+}
---
+++ app/src/main/java/com/rage/quizzmesiapps/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.rage.quizzmesiapps;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.Bundle;
+import android.widget.VideoView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    VideoView videoView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        videoView = (VideoView) findViewById(R.id.videoViewRelative);
+
+        Uri video = Uri.parse("android.resource://" + getPackageName() + "/" + R.raw.quizz_mesi);
+        videoView.setVideoURI(video);
+
+        videoView.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            public void onCompletion(MediaPlayer mp) {
+                startNextActivity();
+            }
+        });
+
+        videoView.start();
+    }
+
+    private void startNextActivity() {
+        if (isFinishing())
+            return;
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
+    }
+}
